c9b039ade61b665f8a3b57480484fbbba8f19416
Troidem <onlinexmax@gmail.com>
2025-10-15 14:09:12 +0300

    feat: move to flurl

--- src/client.rs
+++ src/client.rs
@@ -1,49 +1,54 @@
 use crate::model::CreateOrderRequest;
-use reqwest::header::{HeaderMap, HeaderValue};
+use flurl::{body::FlUrlBody, FlUrl};
 
 pub const API_URL_PROD: &str = "https://api.hyros.com/v1/api/v1.0";
 
 pub struct HyrosApiClient {
     api_url: String,
-    api_key: HeaderValue,
-    client: reqwest::Client,
+    api_key: String,
 }
 
 impl HyrosApiClient {
     pub fn new(api_key: &str, api_url: impl Into<String>) -> Self {
         Self {
             api_url: api_url.into(),
-            client: reqwest::Client::new(),
-            api_key: HeaderValue::from_str(api_key).unwrap(),
+            api_key: api_key.into(),
         }
     }
 
     pub async fn create_order<'a>(&self, request: &CreateOrderRequest<'a>) -> Result<(), String> {
         let url = format!("{}/orders", self.api_url);
-        let request_json = serde_json::to_string(request).unwrap();
-        let res = self
-            .client
-            .post(&url)
-            .headers(self.build_headers())
-            .body(request_json)
-            .send()
-            .await
-            .map_err(|e| e.to_string())?;
-        let status = res.status();
-        let body = res.text().await.unwrap_or_default();
+        let flurl = flurl::FlUrl::new(&url);
+        let flurl = self.add_headers(flurl);
+        let body = FlUrlBody::as_json(request);
+        let result = flurl.post(body).await;
 
-        if status.is_success() && body.contains("OK") {
+        let Ok(resp) = result else {
+            return Err(format!(
+                "FlUrl failed to receive_body: Url: {} {:?}",
+                url,
+                result.unwrap_err()
+            )
+            .into());
+        };
+
+        let result = resp.receive_body().await;
+
+        let Ok(body_bytes) = result else {
+            return Err(format!("FlUrl failed to receive_body: {:?}", result.unwrap_err()).into());
+        };
+
+        let body_str = String::from_utf8(body_bytes).unwrap();
+        if body_str.contains("OK") {
             Ok(())
         } else {
-            Err(format!("HTTP {}: {}", status, body))
+            Err(format!("HTTP {}: {}", url, body_str))
         }
     }
 
-    fn build_headers(&self) -> HeaderMap {
-        let mut headers = HeaderMap::new();
-        headers.insert("Content-Type", HeaderValue::from_static("application/json"));
-        headers.insert("API-Key", self.api_key.clone());
-
-        headers
+    fn add_headers(&self, flurl: FlUrl) -> FlUrl {
+        flurl
+            .with_header("Content-Type", "application/json")
+            .with_header("API-Key", self.api_key.clone())
     }
 }
